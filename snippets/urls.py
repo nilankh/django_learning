cc440592770b291dfd4b5ddf3500ad18c9db7227
nilank nikhil <nilanknikhil@gmail.com>
2024-11-22 20:46:10 +0530

    class based

--- snippets/urls.py
+++ snippets/urls.py
@@ -7,13 +7,26 @@
 # ]
 
 
+# use in fucntion based
+# from django.urls import path
+# from rest_framework.urlpatterns import format_suffix_patterns
+# from snippets import views
+
+# urlpatterns = [
+#     path("snippets/", views.snippet_list),
+#     path("snippets/<int:pk>/", views.snippet_detail),
+# ]
+
+# urlpatterns = format_suffix_patterns(urlpatterns)
+
+# use in class based
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
-    path("snippets/", views.snippet_list),
-    path("snippets/<int:pk>/", views.snippet_detail),
+    path("snippets/", views.SnippetList.as_view()),
+    path("snippets/<int:pk>/", views.SnippetDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
