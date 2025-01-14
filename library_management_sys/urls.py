547904e8a758e30c5fd15929eb97a417f33bb596
nilank nikhil <nilanknikhil@gmail.com>
2025-01-14 19:53:41 +0530

    Library basic crud functionality testing.

--- library_management_sys/urls.py
+++ library_management_sys/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import BookViewSet, MemberViewSet
+from .views import BookViewSet, MemberViewSet, LoanViewSet
 
 
 router = DefaultRouter()
 router.register(r"books", BookViewSet, basename="book")
 router.register(r"members", MemberViewSet, basename="member")
+router.register(r"loans", LoanViewSet, basename="loan")
 
 
 urlpatterns = [path("", include(router.urls))]
--- library_management_sys/views.py
+++ library_management_sys/views.py
@@ -1,15 +1,34 @@
 from rest_framework import viewsets
 from .models import Book, Member, Loan
-from .serializers import BookSerializer, MemberSerializer
+from .serializers import BookSerializer, MemberSerializer, LoanSerializer
 from rest_framework import permissions
 from rest_framework.permissions import AllowAny
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [AllowAny]
 
+
 class MemberViewSet(viewsets.ModelViewSet):
     queryset = Member.objects.all()
     serializer_class = MemberSerializer
     permission_classes = [AllowAny]
+
+
+class LoanViewSet(viewsets.ModelViewSet):
+    queryset = Loan.objects.all()
+    serializer_class = LoanSerializer
+    permission_classes = [AllowAny]
+
+    @action(detail=True, methods=['patch'])
+    def mark_as_returned(self, request, pk=None):
+        loan = self.get_object()
+        print('line 29', loan)
+        return
+        loan.returned = True
+        loan.return_date = request.data.get("return_date")
+        loan.save()
+        return Response({"status": "Loan marked as returned."})
